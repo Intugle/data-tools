9d43bdc7a3e491642604fb943fb61a59c6737b94
juhel-phanju-intugle <juhel@intugle.ai>
2025-08-13 12:54:26 +0530

    created parser, integrated query generator and create mcp

---
+++ tests/mcp/test_semantic_layer.py
@@ -0,0 +1,89 @@
+import functools
+
+from enum import StrEnum
+
+import pytest
+import pytest_asyncio
+
+from mcp import ClientSession
+from mcp.client.streamable_http import streamablehttp_client
+
+URL = "http://127.0.0.1:8080/semantic_layer/mcp"
+
+
+class Tools(StrEnum):
+    GET_TABLES = "get_tables"
+    GET_SCHEMA = "get_schema"
+    EXECUTE_QUERY = "execute_query"
+
+    def __repr__(self) -> str:
+        return self.value
+
+
+def connection_decorator():
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            async with streamablehttp_client(URL) as (read, write, _):
+                async with ClientSession(read, write) as session:
+                    await session.initialize()
+                    return await func(*args, **kwargs, session=session)
+
+        return wrapper
+
+    return decorator
+
+
+class MCPTools:
+    def __init__(self): ...
+
+    @connection_decorator()
+    async def get_tables(self, session: ClientSession = None):
+        """
+        Testing getting tables from the mcp server
+        Args:
+            session (ClientSession, optional): mcp client session
+
+        Returns:
+            bool: flag to indicate whether test passed or not
+        """
+        tables = await session.call_tool(
+            name=Tools.GET_TABLES,
+        )
+
+        assert isinstance(tables.structuredContent["result"], list)
+
+        # assert len(tables.strip()) > 0, "No tables information fetched"
+
+    @connection_decorator()
+    async def get_schema(self, session: ClientSession = None):
+        schemas = await session.call_tool(name=Tools.GET_SCHEMA, arguments={"table_names": ["orders", "products"]})
+        assert isinstance(schemas.structuredContent, dict)
+
+    @connection_decorator()
+    async def execute_query(self, session: ClientSession = None):
+        data = await session.call_tool(
+            name=Tools.EXECUTE_QUERY, arguments={"sql_query": "SELECT * FROM orders LIMIT 10"}
+        )
+        breakpoint()
+        # assert isinstance(schemas.structuredContent, dict)
+
+
+@pytest_asyncio.fixture
+async def mcp_server_tools() -> MCPTools:
+    return MCPTools()
+
+
+@pytest.mark.asyncio
+async def test_mcp_get_tables(mcp_server_tools):
+    await mcp_server_tools.get_tables()
+
+
+@pytest.mark.asyncio
+async def test_mcp_get_schema(mcp_server_tools):
+    await mcp_server_tools.get_schema()
+
+
+@pytest.mark.asyncio
+async def test_mcp_execute_query(mcp_server_tools):
+    await mcp_server_tools.execute_query()
